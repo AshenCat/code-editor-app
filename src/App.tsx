e5c76c36c74167c07158deb6c57d7e31ddd968f4
ashencat <forddagujar@gmail.com>
2021-07-30 20:25:24 -0400

    catstagram base

---
+++ src/App.tsx
@@ -0,0 +1,20 @@
+import './index.scss'
+
+export const App = () => {
+    return <div id="page">
+        <div className="container">
+            <div className="testimonials">
+                <div className="dialog">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Esse, excepturi?</div>
+                
+            </div>
+            <div className="testimonials">
+                <div className="dialog">Sequi fugiat dicta consequatur. Officiis nisi odit tenetur vitae quasi.</div>
+
+            </div>
+            <div className="testimonials">
+                <div className="dialog">Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe quibusdam assumenda tempora architecto. Aut, harum.</div>
+                
+            </div>
+        </div>
+    </div>
+}
