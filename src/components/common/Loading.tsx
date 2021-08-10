d97e3639cad624812c8110c5c4412b396f4e1782
ashencat <forddagujar@gmail.com>
2021-08-10 19:08:08 -0400

    basic loading

---
+++ src/components/common/Loading.tsx
@@ -0,0 +1,10 @@
+import { LinearProgress } from '@material-ui/core'
+import React from 'react'
+
+const Loading = () => {
+    return (
+        <LinearProgress />
+    )
+}
+
+export default Loading
