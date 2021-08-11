75dcfdc7950725a2d9a1eddc59da3148fcf385d5
ashencat <forddagujar@gmail.com>
2021-08-11 19:17:03 -0400

    moved files

--- src/components/common/Header/Header.tsx
+++ src/components/common/Header/Header.tsx
@@ -1,7 +1,7 @@
 import { useAuth0 } from '@auth0/auth0-react'
 import { AppBar, makeStyles, Switch, Toolbar, Typography } from '@material-ui/core'
 import React from 'react'
-import { toggleDarkMode } from '../../../state/dark-mode/reducer';
+import { toggleDarkMode } from '../../../state/reducers/dark-mode/reducer';
 import { useAppDispatch, useAppSelector } from '../../../state/hooks';
 import DarkModeIcon from '@material-ui/icons/Brightness2'
 import UnauthenticatedButtons from './UnauthenticatedButtons';
